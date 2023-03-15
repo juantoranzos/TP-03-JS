cd89a51533eb8f6b153b81d09be3973ed5b1d876
juantoranzos <toranzos628@gmail.com>
2023-03-14 21:20:33 -0300

    ejercicio3arreglos

--- js/ejercicio3arreglos.js
+++ js/ejercicio3arreglos.js
@@ -1,18 +1,20 @@
-let resultados = new Array(11).fill(0); // Inicializar un array con 11 posiciones, de 2 a 12
+/**
+ Escribir un script que simule el lanzamiento de dos dados. Hacer uso de la función Math.random para obtener números aleatorios entre 1 y 6 para cada uno de los lanzamientos de los dados. Sumar el resultado de lanzar dos dados y anotar en un array el número de apariciones de dicha suma, repitiendo 50 veces esta operación.
+
+ */
+
+
+let resultados = new Array(11).fill(0); 
 
 for (let i = 0; i < 50; i++) {
-  // Generar dos números aleatorios entre 1 y 6
+ 
   let dado1 = Math.floor(Math.random() * 6) + 1;
   let dado2 = Math.floor(Math.random() * 6) + 1;
   
-  // Sumar los resultados de los dos dados
   let suma = dado1 + dado2;
-  
-  // Añadir una aparición a la suma correspondiente en el array de resultados
-  resultados[suma - 2]++; // Restar 2 para que el primer índice sea 0 y el último 10
+  resultados[suma - 2]++; 
 }
 
-// Mostrar los resultados por consola
 for (let i = 0; i < resultados.length; i++) {
   document.write(`La suma ${i + 2} apareció ${resultados[i]} veces <br>`);
 }
